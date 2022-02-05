9d2597ceefdf378c51f9b27af5b5a269eaad8a69
Mohamadamin Karami <mohamadaminkarami.ce@gmail.com>
2022-02-05 18:51:01 +0330

    create patient

--- project.py
+++ project.py
@@ -1,13 +1,54 @@
-from time import timezone
-import psycopg
+import psycopg2
+import psycopg2.extras
 
-cursor = psycopg.connect(
+connection = psycopg2.connect(
     dbname="laboratory",
     password="1234",
     user="mohamadamin",
     host="localhost",
     port=5432,
-).cursor()
+)
+cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+
+def create_patient(
+    national_id,
+    fname,
+    lname,
+    gender,
+    bithday,
+    is_married,
+    phonenumber,
+    street,
+    alley,
+    no,
+    insurance_name,
+    insurance_exp_date,
+    weight,
+    height,
+):
+    cursor.execute(
+        "INSERT INTO Person VALUES(%(national_id)s, %(fname)s, %(lname)s, %(gender)s, %(birthday)s, %(is_married)s, %(phonenumber)s, %(street)s, %(alley)s, %(no)s);"
+        + "INSERT INTO Patient VALUES(%(national_id)s, %(insurance_name)s, %(insurance_exp_date)s , %(weight)s, %(height)s);",
+        {
+            "national_id": national_id,
+            "fname": fname,
+            "lname": lname,
+            "gender": gender,
+            "birthday": bithday,
+            "is_married": is_married,
+            "phonenumber": phonenumber,
+            "street": street,
+            "alley": alley,
+            "no": no,
+            "insurance_name": insurance_name,
+            "insurance_exp_date": insurance_exp_date,
+            "weight": weight,
+            "height": height,
+        },
+    )
+
+    connection.commit()
 
 
 def get_patient_receipt(patient_id, start_date, end_date):
@@ -17,20 +58,7 @@ def get_patient_receipt(patient_id, start_date, end_date):
         {"patient_id": patient_id, "start_date": start_date, "end_date": end_date},
     )
 
-    query = []
-
-    for q in cursor.fetchall():
-        query.append(
-            {
-                "Repceip_id": q[0],
-                "Prescription_id": q[1],
-                "patient_id": q[2],
-                "total_cost": q[3],
-                "prepration_date": q[4],
-            }
-        )
-
-    return query
+    return convert_to_dict(cursor.fetchall())
 
 
 def get_experimenter_results(experimenter_id, start_date, end_date):
@@ -43,22 +71,36 @@ def get_experimenter_results(experimenter_id, start_date, end_date):
         },
     )
 
-    query = []
+    return convert_to_dict(cursor.fetchall())
 
-    for q in cursor.fetchall():
-        query.append(
-            {
-                "ExperimenterId": q[0],
-                "ReceiptId": q[1],
-                "PrescriptionId": q[2],
-                "SampleId": q[3],
-                "ExperimentDate": q[4],
-                "Description": q[5],
-                "Comment": q[6],
-            }
-        )
+
+def convert_to_dict(arr):
+    query = []
+    for a in arr:
+        q = {}
+        print(a)
+        for b in a:
+            q[b] = a[b]
+        query.append(q)
 
     return query
 
 
-print(get_patient_receipt("1", "2021-2-2", "2023-2-2"))
+def get_experimenters_patients(experimenter_id, start_date, end_date):
+
+    cursor.execute(
+        "SELECT * FROM Patient INNER JOIN Person WHERE NationalId IN "
+        + "(SELECT PatientId FROM Prescription WHERE PrescriptionId IN "
+        + "(SELECT PrescriptionId FROM Result WHERE ExperimenterId=(%(experimenter_id)s) AND ExperimentDate > (%(start_date)s) AND ExperimentDate < (%(end_date)s)))",
+        {
+            "experimenter_id": experimenter_id,
+            "start_date": start_date,
+            "end_date": end_date,
+        },
+    )
+
+    return convert_to_dict(cursor.fetchall())
+
+
+cursor.execute("SELECT * FROM Person")
+print(cursor.fetchall())
