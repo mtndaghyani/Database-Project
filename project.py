92f6ea33a89ff91653bfee184198cdb87092871d
Mohamadamin Karami <mohamadaminkarami.ce@gmail.com>
2022-02-06 15:27:19 +0330

    implement queries

--- project.py
+++ project.py
@@ -11,6 +11,23 @@ connection = psycopg2.connect(
 cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
 
+def __convert_to_dict(selected_rows):
+    rows = []
+    for row in selected_rows:
+        dict_row = {}
+        for key in row:
+            dict_row[key] = row[key]
+        rows.append(dict_row)
+    return rows
+
+
+def calculate_work_hours():
+    cursor.execute(
+        'SELECT EmployeeId, SUM(EXTRACT(HOUR FROM "End") - EXTRACT(HOUR FROM "Start")) AS workHoursInWeek FROM WorkDay GROUP BY EmployeeId'
+    )
+    return __convert_to_dict(cursor.fetchall())
+
+
 def create_patient(
     national_id,
     fname,
@@ -51,14 +68,40 @@ def create_patient(
     connection.commit()
 
 
+def update_person(national_id, updates):
+    s = ""
+    for update in updates:
+        if type(updates[update]) == str:
+            s += f"{update}='{updates[update]}',"
+        else:
+            s += f"{update}={updates[update]},"
+
+    cursor.execute(
+        "UPDATE Person SET " + s[:-1] + "WHERE NationalId = %(national_id)s",
+        {"national_id": national_id},
+    )
+
+    connection.commit()
+
+
+def get_person_info(national_id):
+
+    cursor.execute(
+        "SELECT * FROM Person WHERE NationalId=%(national_id)s",
+        {"national_id": national_id},
+    )
+
+    return __convert_to_dict(cursor.fetchall())
+
+
 def get_patient_receipt(patient_id, start_date, end_date):
 
     cursor.execute(
-        "SELECT * FROM receipt WHERE PatientId=(%(patient_id)s) AND PreparationDate > (%(start_date)s) AND PreparationDate < (%(end_date)s)",
+        "SELECT * FROM Receipt WHERE PatientId=(%(patient_id)s) AND PreparationDate > (%(start_date)s) AND PreparationDate < (%(end_date)s)",
         {"patient_id": patient_id, "start_date": start_date, "end_date": end_date},
     )
 
-    return convert_to_dict(cursor.fetchall())
+    return __convert_to_dict(cursor.fetchall())
 
 
 def get_experimenter_results(experimenter_id, start_date, end_date):
@@ -71,25 +114,13 @@ def get_experimenter_results(experimenter_id, start_date, end_date):
         },
     )
 
-    return convert_to_dict(cursor.fetchall())
-
-
-def convert_to_dict(arr):
-    query = []
-    for a in arr:
-        q = {}
-        print(a)
-        for b in a:
-            q[b] = a[b]
-        query.append(q)
-
-    return query
+    return __convert_to_dict(cursor.fetchall())
 
 
 def get_experimenters_patients(experimenter_id, start_date, end_date):
 
     cursor.execute(
-        "SELECT * FROM Patient INNER JOIN Person WHERE NationalId IN "
+        "SELECT * FROM Patient INNER JOIN Person ON Patient.NationalId=Person.NationalId WHERE NationalId IN "
         + "(SELECT PatientId FROM Prescription WHERE PrescriptionId IN "
         + "(SELECT PrescriptionId FROM Result WHERE ExperimenterId=(%(experimenter_id)s) AND ExperimentDate > (%(start_date)s) AND ExperimentDate < (%(end_date)s)))",
         {
@@ -99,8 +130,4 @@ def get_experimenters_patients(experimenter_id, start_date, end_date):
         },
     )
 
-    return convert_to_dict(cursor.fetchall())
-
-
-cursor.execute("SELECT * FROM Person")
-print(cursor.fetchall())
+    return __convert_to_dict(cursor.fetchall())
